ddb876235547eb7dab2648cb2d8409221bae7716
gulnoor <gulnoor5233@gmail.com>
2023-12-21 23:19:20 +0500

    3.13 - 3.18

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-let contacts = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-64231222",
-  },
-];
 require("dotenv").config();
 const express = require("express");
-const dexter = require("morgan");
 const cors = require("cors");
-
+const dexter = require("morgan");
 dexter.token("body", (req) => JSON.stringify(req.body));
+
 const app = express();
 const ContactModel = require("./models/contact");
 app.use(cors());
@@ -33,52 +11,84 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(dexter(":method :url :body"));
 
-app.get("/api/persons", (request, response) => {
-  ContactModel.find({}).then((all) => {
-    response.json(all);
-  });
+//create
+app.post("/api/persons", (request, response, next) => {
+  const contact = request.body;
+  ContactModel.findOneAndUpdate({ name: contact.name }, contact, {
+    new: "true",
+  })
+    .then((result) => {
+      if (result) {
+        response.json(result);
+      } else if (contact.name && contact.number) {
+        const c = new ContactModel(contact);
+        c.save()
+          .then((savedc) => response.json(savedc))
+          .catch((err) => next(err));
+      } else {
+        response
+          .status(404)
+          .json({ error: "Enter details before creating contact" });
+      }
+    })
+    .catch((e) => next(e));
 });
 
-app.post("/api/persons", (request, response) => {
-  const contact = { ...request.body };
+//read
+app.get("/api/persons", (request, response, next) => {
+  ContactModel.find({})
+    .then((all) => {
+      response.json(all);
+    })
+    .catch((err) => next(err));
+});
+app.get("/api/:id", (request, response, next) => {
+  ContactModel.findById(request.params.id)
+    .then((searchResult) => {
+      searchResult
+        ? response.json(searchResult)
+        : response.status(404).json({ error: "contact not found" });
+    })
+    .catch((err) => next(err));
+});
+// app.get("/info", (request, response) => {
+//   response.send(`Phonebook has info for ${contacts.length} people
+//   ${new Date()}`);
+// });
 
-  if (contact.name && contact.number) {
-    const c = new ContactModel(contact);
-    c.save().then((savedc) => response.json(savedc));
+//update
+app.put("/api/persons/:id", (request, response, next) => {
+  const contact = request.body;
+
+  if (contact && contact.number) {
+    ContactModel.findByIdAndUpdate(request.params.id, contact, {
+      new: true,
+    })
+      .then((newContact) => response.json(newContact))
+      .catch((err) => next(err));
   } else {
-    response.status(404).json({ error: "bruh..." });
+    response
+      .status(404)
+      .json({ error: "enter contact details before updating" });
   }
 });
-app.put("/api/persons/:id", (request, response) => {
-  const id = parseInt(request.params.id);
-  const contact = { ...request.body, id };
 
-  if (contact.name && contact.number) {
-    contacts = contacts.filter((c) => c.id !== id);
-    contacts = contacts.concat(contact);
-    response.json(contact);
-  } else {
-    response.status(404).json({ error: "name must be unique" });
+//delete
+app.delete("/api/:id", (request, response, next) => {
+  ContactModel.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).send("deleted successfully"))
+    .catch((err) => next(err));
+});
+
+//error handling middleware
+const errorhandler = (error, req, res, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
   }
-});
-
-app.get("/api/:id", (request, response) => {
-  const id = parseInt(request.params.id);
-  const contact = contacts.find((contact) => contact.id === id);
-  contact ? response.json(contact) : response.status(404).send();
-});
-
-app.delete("/api/:id", (request, response) => {
-  console.log(request.params.id);
-  const id = parseInt(request.params.id);
-  contacts = contacts.filter((c) => c.id !== id);
-  response.json(contacts);
-});
-
-app.get("/info", (request, response) => {
-  response.send(`Phonebook has info for ${contacts.length} people
-  ${new Date()}`);
-});
+  next(error);
+};
+app.use(errorhandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
